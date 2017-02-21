259f954a20c5cc477763d56c62e530b5e0c9316f
Magnus Wallström <magnus.wallstrom@sandvik.com>
2017-02-21 13:20:48 +0100

    Little bit more

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,31 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CameraComponent } from './camera/camera.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'camera', component: CameraComponent },
+  { path: '', component: LoginComponent },
+  { path: '**', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    CameraComponent
+    CameraComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     MaterialModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
